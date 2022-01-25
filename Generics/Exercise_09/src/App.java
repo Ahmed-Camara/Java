4a657e8c13fe6e8007239ead58f28e4df5f7bbdb
Ahmed-Camara <camarahmed123@gmail.com>
2022-01-25 18:30:17 +0000

    chapter 19 Generic : Exercise 09

--- Generics/Exercise_09/src/App.java
+++ Generics/Exercise_09/src/App.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> p = new ArrayList<>();
+        p.add(12);
+        p.add(2);
+        p.add(32);
+        p.add(4);
+        p.add(52);
+        p.add(53);
+        p.add(54);
+        p.add(55);
+        System.out.println(p);
+        sort(p);
+
+        System.out.println(p);
+    }
+
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list){
+        sort(list,0,list.size()-1);
+    }
+
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list,int low, int high){
+
+        if(low > high)
+            return;
+        E max = list.get(low);
+        int maxIndex = low;
+        for(int i = low+1; i <= high; i++){
+            if(max.compareTo(list.get(i)) > 0){
+                max = list.get(i);
+                maxIndex = i;
+            }
+        }
+
+        list.set(maxIndex,list.get(low));
+        list.set(low,max);
+
+        sort(list,low+1,high);
     }
 }
