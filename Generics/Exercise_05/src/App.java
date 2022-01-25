75e97158acf0aad605104e4827ed1de4f619e0d8
Ahmed-Camara <camarahmed123@gmail.com>
2022-01-25 05:12:06 +0000

    chapter 19 Generic : Exercise 05

--- Generics/Exercise_05/src/App.java
+++ Generics/Exercise_05/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) {
-        
+        Integer[] list = {1,2,3,4,5,100};
+        System.out.println(max(list));
+    }
+    public static <E extends Comparable<E>> E max(E[]list){
+        E item = list[0];
+        E max = null;
+
+        for(int i = 0; i < list.length; i++){
+
+            if(list[i].compareTo(item) > 0)
+                max = list[i];
+        }
+
+        return max;
     }
 }
