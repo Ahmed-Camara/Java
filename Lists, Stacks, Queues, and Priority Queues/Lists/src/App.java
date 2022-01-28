824a89f5faae2b101468614609eaca8adce980cc
Ahmed-Camara <camarahmed123@gmail.com>
2022-01-28 09:30:58 +0600

    Lists

--- Lists, Stacks, Queues, and Priority Queues/Lists/src/App.java
+++ Lists, Stacks, Queues, and Priority Queues/Lists/src/App.java
@@ -1,6 +1,9 @@
 /*
 * List interface extends the Collection interface and defines a collection for storing elements in a sequential order.
 * List is created using two concrete classes : ArrayList or LinkedList.
+* ArrayList offers better performance when we want random access through an index without inserting or removing elements
+    at the beginning of the list.
+*  LinkedList offers better performance if we want to to perform insertion or deletion of elements at the beginning of the list.
 * The listIterator() listIterator(startIndex) method returns an instance of ListIterator which extends the Iterator interface
     to add bidirectional traversal of the list.
     - Methods in the ListIterator interface :
