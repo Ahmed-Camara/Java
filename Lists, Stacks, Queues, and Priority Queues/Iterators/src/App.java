6b8dd96622fa398b7b0a531e4f08cbcdfc4bd3b0
Ahmed-Camara <camarahmed123@gmail.com>
2022-01-28 09:04:45 +0600

    Iterators

---
+++ Lists, Stacks, Queues, and Priority Queues/Iterators/src/App.java
@@ -0,0 +1,12 @@
+/*
+* Each collection is Iterable, we can obtain its Iterator object to traverse all the elements in the collection.
+* Iterator allows to traverse a data structure without having to expose the details of how data is stored in the data structure.
+* The Collection interface extends the Iterable interface which defines the iterator() method, which returns an iterator.
+* The iterable interface returns an instance of Iterator which provides access to the next() method to access elements in the collection, the hasNext() method to check whether there are more elements in the iterator, and the remove() method to remove the last element returned by the iterator.
+* */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
