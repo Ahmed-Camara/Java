da772515d352d57ec7ba2b5563bd59b437271f39
Ahmed-Camara <camarahmed123@gmail.com>
2022-02-02 01:42:18 +0600

    factorial

---
+++ Dynamic Programming/Factorial/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int numTest;
+        Scanner in = new Scanner(System.in);
+        numTest = in.nextInt();
+        int []tab = new int[numTest+1];
+
+        for (int i = 0; i < tab.length; i++)
+            tab[i] = -1;
+        int []res = new int[numTest+1];
+        for (int i = 0; i < numTest; i++){
+            res[i] = factorial(i+1,tab);
+        }
+
+        for (int i = 0; i < res.length-1; i++){
+            System.out.println(res[i]);
+        }
+    }
+    public static int factorial(int x,int[] tab){
+        if(x <= 2)
+            return x;
+
+        if(tab[x] != -1)
+            return tab[x];
+
+        tab[x] = x * factorial(x-1,tab);
+        return tab[x];
+    }
+}
