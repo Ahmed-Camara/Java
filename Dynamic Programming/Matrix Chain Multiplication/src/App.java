74ad52518aed0a05e94c089fd4807e5b3cb82f85
Ahmed-Camara <camarahmed123@gmail.com>
2022-02-04 02:59:22 +0600

    matrix chain multiplication

--- Dynamic Programming/Matrix Chain Multiplication/src/App.java
+++ Dynamic Programming/Matrix Chain Multiplication/src/App.java
@@ -1,33 +1,41 @@
 public class App {
     public static void main(String[] args) {
-        int[][] a = {{1,2,3},{4,5,6}};
-        int[][] b = {{1,2,3},{4,5,6},{7,8,9}};
-        int [][] c = matrix_multiply(a,b);
-
-        for(int i = 0; i < c.length; i++){
-            for(int j = 0; j < c[0].length; j++){
-                System.out.print(c[i][j]+" ");
-            }
-            System.out.println();
-        }
+        int arr[] = new int[] { 1, 2, 3, 4 };
+        System.out.println("Minimum number of multiplications is "
+                + memoizedMatrixChain(arr));
     }
 
-    public static int[][] matrix_multiply(int[][]a, int[][] b){
-        //if # of cols of a does not match with the # of rows of b
-        if(a[0].length != b.length){
-            throw new RuntimeException("Matrices dimension do not match");
-        }
+    public static int memoizedMatrixChain(int []p){
+        int n = p.length;
+        int [][]m = new int[n][n];
 
-        int[][] c = new int[a.length][b[0].length];
-
-        for(int i = 0; i < a.length; i++){
-            for(int j = 0; j < b[0].length; j++){
-                c[i][j] = 0;
-                for(int k = 0; k < a[0].length; k++){
-                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
-                }
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                m[i][j] = Integer.MAX_VALUE;
             }
         }
-        return c;
+
+        return lookupChain(m,p,1,n-1);
+    }
+
+    public static int lookupChain(int [][]m, int []p, int i, int j){
+        if (m[i][j] < Integer.MAX_VALUE)
+            return m[i][j];
+
+        if (i == j)
+            m[i][j] = 0;
+
+        else{
+            for (int k = i; k < j-1; k++){
+                int q = lookupChain(m,p,i,k) + lookupChain(m,p,k+1,j)+p[i-1] * p[k] * p[j];
+
+                if(q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+
+
+
+        return m[i][j];
     }
 }
