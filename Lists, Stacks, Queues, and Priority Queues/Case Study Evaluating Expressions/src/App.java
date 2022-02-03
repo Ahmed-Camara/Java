6ee9e2827569959d497f2d7a47a0eabaaadf6e87
Ahmed-Camara <camarahmed123@gmail.com>
2022-02-03 17:12:58 +0600

    Evaluate expressions

--- Lists, Stacks, Queues, and Priority Queues/Case Study Evaluating Expressions/src/App.java
+++ Lists, Stacks, Queues, and Priority Queues/Case Study Evaluating Expressions/src/App.java
@@ -6,7 +6,7 @@ public class App {
         Scanner input = new Scanner(System.in);
         String expressions = input.nextLine();
         try {
-
+            System.out.println(evaluateExpression(expressions));
         }catch (Exception e){
             System.out.println("Wrong expression : "+expressions);
         }
@@ -15,5 +15,68 @@ public class App {
     public static int evaluateExpression(String expression){
         Stack<Integer> operandStack = new Stack<>();
         Stack<Character> operatorStack = new Stack<>();
+
+        expression = insertBlanks(expression);
+        String[] tokens = expression.split(" ");
+
+        for(String token : tokens){
+            if(token.length() == 0) continue;
+            else if(token.charAt(0) == '+' || token.charAt(0) == '-'){
+                // procees all the operators at the top of operatorStack
+                while (!operatorStack.isEmpty() && (operatorStack.peek() == '+' || operatorStack.peek() == '-'||
+                        operatorStack.peek() == '*' || operatorStack.peek() == '/')){
+                    processAnOperator(operandStack,operatorStack);
+                }
+                operatorStack.push(token.charAt(0));
+
+            }else if(token.charAt(0) == '*' || token.charAt(0) == '/'){
+                while (!operatorStack.isEmpty() && (operatorStack.peek() == '*' || operatorStack.peek() == '/')){
+                    processAnOperator(operandStack,operatorStack);
+                }
+                operatorStack.push(token.charAt(0));
+            }else if(token.trim().charAt(0) == '('){
+                operatorStack.push('(');
+            }else if(token.trim().charAt(0) == ')'){
+                while (operatorStack.peek() != '('){
+                    processAnOperator(operandStack,operatorStack);
+                }
+                operatorStack.pop();
+            }else{
+                operandStack.push(new Integer(token));
+            }
+        }
+
+        while (!operatorStack.isEmpty()){
+            processAnOperator(operandStack,operatorStack);
+        }
+        return operandStack.pop();
+    }
+    public static void processAnOperator(Stack<Integer> operandStack, Stack<Character> operatorStack){
+        char op = operatorStack.pop();
+        int op1 = operandStack.pop();
+        int op2 = operandStack.pop();
+
+        if(op == '+'){
+            operandStack.push(op2 + op1);
+        }else if(op == '-'){
+            operandStack.push(op2 - op1);
+        }else if(op == '*'){
+            operandStack.push(op2 * op1);
+        }else if(op == '/'){
+            operandStack.push(op2 / op1);
+        }
+    }
+    public static String insertBlanks(String s){
+        String result = "";
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '(' || s.charAt(i) == ')' ||
+                s.charAt(i) == '+' || s.charAt(i) == '-' ||
+                s.charAt(i) == '*' || s.charAt(i) == '/'){
+                result += " "+s.charAt(i)+" ";
+            }else{
+                result += s.charAt(i);
+            }
+        }
+        return  result;
     }
 }
