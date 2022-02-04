08da2bb1225641d5f2c0f0f555c4ba86c4a4786e
Ahmed-Camara <camarahmed123@gmail.com>
2022-02-05 02:18:55 +0600

    Exercise 09

---
+++ Lists, Stacks, Queues, and Priority Queues/Exercise_09/src/main/java/com/example/exercise_09/App.java
@@ -0,0 +1,53 @@
+package com.example.exercise_09;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage){
+        BorderPane pane = new BorderPane();
+        MultipleBallPlane ballPane = new MultipleBallPlane();
+
+        ScrollBar sbSpeed = new ScrollBar();
+        sbSpeed.setMax(20);
+        sbSpeed.setValue(10);
+        ballPane.rateProperty().bind(sbSpeed.valueProperty());
+
+        Button btAdd = new Button("+");
+        Button btSubstract = new Button("-");
+        Button btSuspend = new Button("Suspend");
+        Button btResume = new Button("Resume");
+
+        btAdd.setOnAction(e->ballPane.add());
+        btSubstract.setOnAction(e->ballPane.substract());
+        btSuspend.setOnAction(e->ballPane.suspend());
+        btResume.setOnAction(e->ballPane.resume());
+        ballPane.setOnMousePressed(e->ballPane.removeBall(e));
+        HBox hBox = new HBox(10);
+        hBox.setPadding(new Insets(10));
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getChildren().addAll(btAdd,btSubstract,btSuspend,btResume);
+
+        pane.setTop(sbSpeed);
+        pane.setCenter(ballPane);
+        pane.setBottom(hBox);
+        Scene scene = new Scene(pane,800, 700);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Lists, Stacks, Queues, and Priority Queues/Exercise_09/src/main/java/com/example/exercise_09/MultipleBallPlane.java
@@ -0,0 +1,101 @@
+package com.example.exercise_09;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.Event;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.util.*;
+
+public class MultipleBallPlane extends Pane {
+    private Timeline animation = null;
+    private PriorityQueue<Ball> pq = null;
+    public MultipleBallPlane(){
+        pq = new PriorityQueue<>(6, new Comparator<Ball>() {
+            @Override
+            public int compare(Ball o1, Ball o2) {
+                if (o1.getRadius() > o2.getRadius())
+                    return -1;
+                else if(o1.getRadius() < o2.getRadius())
+                    return 1;
+                else
+                    return 0;
+            }
+        });
+        animation = new Timeline(new KeyFrame(Duration.millis(50), e->moveBall()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
+    }
+
+    public void add(){
+        Color color = new Color(Math.random(),Math.random(),Math.random(),0.5);
+        int radius = (int) (Math.random() * 20) + 2;
+        Ball ball = new Ball(30,30,radius,color);
+        getChildren().add(ball);
+        pq.offer(ball);
+    }
+    public void substract(){
+        if(getChildren().size() > 0){
+            getChildren().remove(pq.poll());
+        }
+    }
+    public void suspend(){
+        animation.pause();
+    }
+    public void resume(){
+        animation.play();
+    }
+    public DoubleProperty rateProperty(){
+        return animation.rateProperty();
+    }
+    private void moveBall(){
+
+        for(int i = 0; i < this.getChildren().size(); i++){
+            Ball ball = (Ball) this.getChildren().get(i);
+
+            if(ball.getCenterX() < ball.getRadius() || ball.getCenterX() > getWidth()-ball.getRadius()){
+                ball.dx *= -1;
+            }
+            if(ball.getCenterY() < ball.getRadius() || ball.getCenterY() > getHeight() - ball.getRadius()){
+                ball.dy *= -1;
+            }
+
+            ball.setCenterX(ball.dx + ball.getCenterX());
+            ball.setCenterY(ball.dy + ball.getCenterY());
+
+            if (this.getChildren().size() > 1){
+                for (int j = 0; j < this.getChildren().size() - 1; j++){
+                    Ball ball1 = (Ball) this.getChildren().get(j);
+                    Ball ball2 = (Ball) this.getChildren().get(j+1);
+
+                    if(ball1.getBoundsInParent().intersects(ball2.getBoundsInParent())){
+                        ball1.setRadius(ball1.getRadius() + ball2.getRadius());
+                        substract();
+                    }
+                }
+            }
+        }
+    }
+
+    public void removeBall(Event e){
+        try {
+            Ball ball = (Ball) e.getTarget();
+            this.getChildren().remove(ball);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    class Ball extends Circle {
+        private double dx=1,dy=1;
+        public Ball(double x, double y, double radius, Color color){
+            super(x,y,radius);
+            setFill(color);
+        }
+    }
+}
+
