8f8e1c84caaa999e6d7cb35ccb35b8b144a76040
Ahmed-Camara <camarahmed123@gmail.com>
2022-02-08 17:09:43 +0600

    Catalan Number

--- Dynamic Programming/Catalan Number/src/App.java
+++ Dynamic Programming/Catalan Number/src/App.java
@@ -1,12 +1,16 @@
 public class App {
+    private static final int[]cat = new int[100];
     public static void main(String[] args) {
 
         //1. Catalan number without dynamic programming
         for (int i = 0; i < 10; i++){
             System.out.print(catalanNumber(i)+" ");
         }
-
+        System.out.println();
         //2. Catalan number with dynamic programming
+        for (int i = 0; i < 10; i++){
+            System.out.print(DPcatalanNumber(i)+" ");
+        }
     }
 
     public static int catalanNumber(int n){
@@ -18,5 +22,17 @@ public class App {
         }
 
         return res;
+    }
+    public static int DPcatalanNumber(int n){
+        cat[0] = cat[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            cat[i] = 0;
+            for (int j = 0; j < i; j++){
+                cat[i] += cat[j] * cat[i-j-1];
+            }
+        }
+
+        return cat[n];
     }
 }
