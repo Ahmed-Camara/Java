2055d05b81eed9c53853ab21d8fb06e9a9194c4b
Ahmed-Camara <camarahmed123@gmail.com>
2022-02-12 00:04:15 +0600

    Exercise 12

--- Lists, Stacks, Queues, and Priority Queues/Exercise_12/src/App.java
+++ Lists, Stacks, Queues, and Priority Queues/Exercise_12/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) {
 
+        MyPriorityQueue<String> cities = new MyPriorityQueue<>();
+        cities.offer("Abidjan");
+        cities.offer("Bouake");
+        cities.offer("Yakro");
+        System.out.println(cities); // [Abidjan, Bouake, Yakro]
+
+        MyPriorityQueue<String> citiesClone = (MyPriorityQueue<String>) cities.clone();
+        System.out.println(citiesClone); // [Abidjan, Bouake, Yakro]
+
+        cities.offer("Odienne");
+        citiesClone.offer("Bouafle");
+
+        System.out.println(cities); // [Abidjan, Bouake, Yakro,Odienne]
+        System.out.println(citiesClone); // [Abidjan, Bouake, Yakro,Bouafle]
     }
 }
--- Lists, Stacks, Queues, and Priority Queues/Exercise_12/src/MyPriorityQueue.java
+++ Lists, Stacks, Queues, and Priority Queues/Exercise_12/src/MyPriorityQueue.java
@@ -3,5 +3,13 @@ import java.util.PriorityQueue;
 public class MyPriorityQueue<E> extends PriorityQueue<E> implements Cloneable {
 
     @Override
-    public Object
+    public Object clone(){
+        MyPriorityQueue<E> obj = new MyPriorityQueue<>();
+
+        for(E o : this){
+            obj.offer(o);
+        }
+
+        return obj;
+    }
 }
