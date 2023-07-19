3bc3f4595634afde21db53e99b6b10a922336755
Ahmed-Camara <camarahmed123@gmail.com>
2023-07-19 10:56:45 +0100

    Exercise_15: Addition quiz

--- Sets And Maps/Exercise_15/src/Main.java
+++ Sets And Maps/Exercise_15/src/Main.java
@@ -1,9 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number1 = (int)(Math.random() * 10);
+		int number2 = (int)(Math.random() * 10);
 
+		Scanner input = new Scanner(System.in);
+
+		Set<Integer> answers = new HashSet<>();
+
+		System.out.print(
+			"What is " + number1 + " + " + number2 + "? ");
+		int answer = input.nextInt();
+
+		while (number1 + number2 != answer) {
+			if (answers.contains(answer))
+				System.out.println("You already entered " + answer);
+			else {
+				System.out.print("Wrong answer. Try again. What is "
+					+ number1 + " + " + number2 + "? ");
+				answers.add(answer);
+			}
+			answer = input.nextInt();
+		}
+
+		System.out.println("You got it!");
 	}
 
 }
